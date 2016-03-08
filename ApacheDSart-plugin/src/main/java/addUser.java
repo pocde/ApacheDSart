30f7b7b42c9c3350dacd2df95a6f59e3362fab0f
pocde <github@poc.de1.cc>
2016-03-08 16:04:52 +0100

    first commit

---
+++ ApacheDSart-plugin/src/main/java/addUser.java
@@ -0,0 +1,12 @@
+import com.hp.oo.sdk.content.annotations.Action;
+import com.hp.oo.sdk.content.annotations.Output;
+import com.hp.oo.sdk.content.annotations.Param;
+import com.hp.oo.sdk.content.annotations.Response;
+import com.hp.oo.sdk.content.constants.OutputNames;
+import com.hp.oo.sdk.content.constants.ResponseNames;
+import com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType;
+import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
+
+public class addUser {
+
+}
