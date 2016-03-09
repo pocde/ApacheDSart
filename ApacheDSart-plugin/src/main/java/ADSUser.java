b2bd53d6f5f865ef970e4059a3a6e25d9a386ecd
pocde <github@poc.de1.cc>
2016-03-09 11:20:03 +0100

    added LDAP connections and objects

--- ApacheDSart-plugin/src/main/java/ADSUser.java
+++ ApacheDSart-plugin/src/main/java/ADSUser.java
@@ -2,11 +2,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.directory.api.ldap.model.entry.DefaultAttribute;
 import org.apache.directory.api.ldap.model.entry.DefaultEntry;
 import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidAttributeValueException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
 import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
+
 
 import com.hp.oo.sdk.content.annotations.Action;
 import com.hp.oo.sdk.content.annotations.Output;
@@ -19,60 +23,137 @@ import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
 
 public class ADSUser {
 
-	@Action(value = "addUserToADS",
-			description = "adds a user to the Apache Directory Server",
-			outputs = { @Output(OutputNames.RETURN_RESULT) },
-			responses = {
-				@Response(text = ResponseNames.SUCCESS, field = OutputNames.RETURN_RESULT, value = "0", matchType = MatchType.COMPARE_GREATER_OR_EQUAL, responseType = ResponseType.RESOLVED),
-				@Response(text = ResponseNames.FAILURE, field = OutputNames.RETURN_RESULT, value = "0", matchType = MatchType.COMPARE_LESS, responseType = ResponseType.ERROR)
-			}
-	)
-	public Map<String,String> addUserToADS (
-			@Param(value = "username") String username,
-			@Param(value = "password") String password,
-			@Param(value = "host") String host,
-			@Param(value = "port") String portStr
-			)
-	{
-		Map<String, String> resultMap = new HashMap<String, String>();
-		int port = 10389;  // default port for Apache Directory Server
-		
+	/*
+	 * firt we handle Ldap connections
+	 */
+	private LdapConnection connection;
+	
+	 LdapConnection getConnection() {
+		 return connection;
+	 }
+		   
+	 void setConnection(LdapConnection connection) {
+		 this.connection = connection;
+	 }
+
+	 public void connectAndBind(String host, int port, String username, String password) throws LdapException, IOException {
+		 setConnection(new LdapNetworkConnection(host, port));
+		 getConnection().bind(username, password);  
+	 }
+
+	 public void unbind() throws LdapException {
+		 getConnection().unBind();
+	 }
+
+	 public void close() throws IOException, LdapException {
+		 getConnection().unBind();
+		 getConnection().close();
+	 }	
+	
+	 /*
+	  * now we handle Ldap entries and organize them
+	  */
+	 private DefaultEntry de;
+	 
+	 public void setEntry(DefaultEntry de) {
+		 this.de = de;
+	 }
+	 
+	 public DefaultEntry getEntry() {
+		 return this.de;
+	 }
+	 
+	 public void newEntry(Dn dn)
+	 {
+		DefaultEntry de;
 		try {
-			port = Integer.valueOf(portStr);
-		} catch (Exception e) {
-			port = 10389;
-		}
-			
-		LdapConnection connection = new LdapNetworkConnection(host, port);
-		try {
-			connection.bind(username, password);
-		} catch (Exception e) {
-			resultMap.put("resultMessage", "server not reachable");
-			resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(1));
-			return resultMap;
-		}
-		
-		/*
-		 * before we return from this step we need to clean up the connection
-		 * to the LDAP server
-		 */
-		try {
-			connection.unBind();
-			connection.close();
+			de = new DefaultEntry(dn, "objectclass: top");
 		} catch (LdapException e) {
-			resultMap.put("resultMessage", "could not unbind connection to LDAP server");
-			resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
-		} catch (IOException e) {
-			resultMap.put("resultMessage", "could not close connection to LDAP server");
-			resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
+			return;
 		}
 		
+		this.setEntry(de);
+	 }
+	 
+	 public void addEntry(String attr, String value) throws LdapException {
+		 getEntry().add(attr, value);
+	 }
+	 
+	 @Action(value = "addUserToADS",
+			 description = "adds a user to the Apache Directory Server",
+			 outputs = { @Output(OutputNames.RETURN_RESULT) },
+			 responses = {
+					 @Response(text = ResponseNames.SUCCESS, field = OutputNames.RETURN_RESULT, value = "0", matchType = MatchType.COMPARE_GREATER_OR_EQUAL, responseType = ResponseType.RESOLVED),
+					 @Response(text = ResponseNames.FAILURE, field = OutputNames.RETURN_RESULT, value = "0", matchType = MatchType.COMPARE_LESS, responseType = ResponseType.ERROR)
+	 }	
+	 )	
+	 public Map<String,String> addUserToADS (
+			 @Param(value = "username") String username,
+			 @Param(value = "password") String password,
+			 @Param(value = "host") String host,
+			 @Param(value = "port") String portStr,
+			 @Param(value = "DN") String dn
+			 )
+	 {
+		 Map<String, String> resultMap = new HashMap<String, String>();
+		 int port = 10389;  // default port for Apache Directory Server
+
+		 try {
+			Dn udn = new Dn(dn);
+		} catch (LdapInvalidDnException e) {
+			 resultMap.put("resultMessage", "could not get DN");
+			 resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(2));
+			 return resultMap;
+		}
 		
-		resultMap.put("resultMessage", "server reachable");
-		resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
+		 /*
+		  * All inputs from an OO step are strings! So we need to convert port 
+		  * to an integer. 
+		  */
+		 try {
+			 port = Integer.valueOf(portStr);
+		 } catch (Exception e) {
+			 port = 10389;
+		 }
+			
+		 /*
+		  * connect to LDAP
+		  */
+		 try {
+			 connectAndBind(host, port, username, password);
+		 } catch (Exception e) {
+			 resultMap.put("resultMessage", "server not reachable");
+			 resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(1));
+			 return resultMap;
+		 }
+		 
+		 /*
+		  * here we start our main code!
+		  */
+		 
+
 		
-		return resultMap;
-	}
+		 /*
+		  * before we return from this step we need to clean up the connection
+		  * to the LDAP server
+		  */
+		 try {
+			 getConnection().unBind();
+			 getConnection().close();
+		 } catch (LdapException e) {
+			 resultMap.put("resultMessage", "could not unbind connection to LDAP server");
+			 resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
+		 } catch (IOException e) {
+			 resultMap.put("resultMessage", "could not close connection to LDAP server");
+			 resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
+		 }
+		
+		
+		 resultMap.put("resultMessage", "server reachable");
+		 resultMap.put(OutputNames.RETURN_RESULT, String.valueOf(0));
+		
+		 return resultMap;
+	 }
 
 	public void addUser(String uid, String givenname, String partyid, String password) 
 
@@ -103,4 +184,6 @@ public class ADSUser {
 			    //connection.add(de);
 			    System.out.println("Entry addition successful"); 
 			  }
+	
+	
 }
--- ApacheDSart-plugin/src/test/java/ADSUserTest.java
+++ ApacheDSart-plugin/src/test/java/ADSUserTest.java
@@ -1,24 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.directory.api.ldap.model.entry.DefaultEntry;
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
+import org.apache.directory.api.ldap.model.name.Dn;
 import org.junit.Test;
 
 public class ADSUserTest {
+	
+	/* set environment */
+	String username="uid=admin,ou=system";
+	String password="secret";
+	String host="codar.poc.de1.cc";
+	String portStr="10389";
+	String dn = "cn=Users,ou=CSAGroups,dc=example,dc=com";
+	
 
 	@Test
 	public void testAddUserToADS() {
 		Map<String,String> result = new HashMap<String,String>();
-		
-		/* set environment */
-		String username="uid=admin,ou=system";
-		String password="secret";
-		String host="codar.poc.de1.cc";
-		String portStr="10389";
-		
+
 		ADSUser user = new ADSUser();
-		result = user.addUserToADS(username, password, host, portStr);
+		result = user.addUserToADS(username, password, host, portStr, dn);
 		
 		if (result.get("returnResult").length() > 1) fail("Return Result < 0");
 		String ret=result.get("returnResult");
@@ -29,6 +36,69 @@ public class ADSUserTest {
 		 * command are working as desired the return value will be 0.
 		 */
 		assertTrue("could not connect", ret.equals("0"));
+	}
+	
+	/*
+	 * the following test only tests if a connection to the LDAP server
+	 * can be established and closed again. There is no further test
+	 * included.
+	 */
+	@Test
+	public void testLdapConnection() {
+		int port=10389;
+		
+		try {
+			port=Integer.valueOf(portStr);
+		} catch (Exception e) {
+			/* do nothing */
+		}
+		
+		ADSUser user = new ADSUser();
+		
+		try {
+			user.connectAndBind(host, port, username, password);
+		} catch (LdapException | IOException e) {
+			fail("connection to Ldap could not be established");
+		}
+		
+		assertTrue("could not get connection", user.getConnection().isConnected());
+		
+		try {
+			user.getConnection().unBind();
+			user.getConnection().close();
+		} catch (IOException | LdapException e) {
+			fail("closing Ldap connection failed.");
+		}
+		
+		assertFalse("could not close connection", user.getConnection().isConnected());
+	}
+	
+	/*
+	 * test objects in Ldap. Create an entry and then add an object.
+	 */
+	@Test
+	public void testObjectEntries() {
+		ADSUser user = new ADSUser();
+		Dn udn = null;
+		
+		try {
+			udn = new Dn(dn);
+		} catch (LdapInvalidDnException e) {
+			fail("could not create DN");
+		}
+		
+		user.newEntry(udn);
+		
+		assertTrue("could not create entry", user.getEntry().getDn().equals(udn));
+		
+		try {
+			user.addEntry("objectclass", "inetOrgPerson");
+		} catch (LdapException e) {
+			fail("could not add entry 'inetOrgPerson'");
+		}
+		
+		assertTrue("could not add entry 'inetOrgPerson'", 
+				user.getEntry().contains("objectclass", "inetOrgPerson"));
 	}
 
 }
